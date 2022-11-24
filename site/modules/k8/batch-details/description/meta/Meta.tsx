24a2fbff0c4167bcdb8395ce1cb579374544926a
Md Amir Gauhar <md.gauhar@pw.live>
2022-11-24 14:37:10 +0000

    Feature/ui

---
+++ site/modules/k8/batch-details/description/meta/Meta.tsx
@@ -0,0 +1,38 @@
+import { Typography } from '@components/ui'
+
+const META = [
+  { id: 1, key: 'Lectures', value: '100+' },
+  { id: 2, key: 'Quizzes', value: '50+' },
+  { id: 3, key: 'Worksheets', value: '20+' },
+]
+
+const Meta = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <Typography variant="subHeading" weight={700}>
+        This Course Includes
+      </Typography>
+      <div className="flex items-center">
+        {META.map(
+          (meta: { id: number; key: string; value: string }, idx: number) => (
+            <div
+              key={meta.id}
+              className={`flex flex-col items-center ${
+                idx <= META.map.length && 'border-r'
+              } ${idx !== 0 && 'pl-6'} ${idx !== META.length - 1 && 'pr-6'}`}
+            >
+              <Typography variant="regular" weight={700}>
+                {meta.value}
+              </Typography>
+              <Typography variant="small" weight={500}>
+                <span className="text-[#999]">{meta.key}</span>
+              </Typography>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default Meta
--- site/modules/k8/home/testimonials/testimonial-card/TestimonialCard.tsx
+++ site/modules/k8/home/testimonials/testimonial-card/TestimonialCard.tsx
@@ -1,35 +1,45 @@
 import user from '@assets/images/user.svg'
-import { Typography } from '@components/ui'
+import { Card, Typography } from '@components/ui'
 import { StarIcon } from '@heroicons/react/24/solid'
 import style from './TestimonialCard.module.css'
 
 const TestimonialCard = ({ testimonialData }: { testimonialData: any }) => {
   return (
-    <div className={style.testimonialCard}>
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img src={user.src} alt="avatar" className="absolute h-[80px] -top-5" />
-      <div className="flex flex-col gap-1 ml-[100px]">
-        <Typography variant="regular" weight={700}>
-          <span className="truncate">{testimonialData.name}</span>
-        </Typography>
-        <div className="flex items-center gap-1">
-          {[1, 2, 3, 4, 5].map((i: any) => (
-            <StarIcon
-              key={i}
-              height={14}
-              width={14}
-              className={`text-[#6a4fe27a] ${i < 4 ? 'text-[#7252F7]' : ''}`}
-            />
-          ))}
+    <Card>
+      <div className={style.testimonialCard}>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src={user.src}
+          alt="avatar"
+          className="absolute h-[68px] md:h-[77px] -top-5"
+        />
+        <div className="flex flex-col gap-1 ml-[100px] max-w-full">
+          <div className="">
+            <Typography variant="regular" weight={700}>
+              <span className="truncate">{testimonialData.name}</span>
+            </Typography>
+          </div>
+          <div className="flex items-center gap-1">
+            {[1, 2, 3, 4, 5].map((i: any) => (
+              <StarIcon
+                key={i}
+                height={14}
+                width={14}
+                className={`text-[#6a4fe27a] ${i < 4 ? 'text-[#7252F7]' : ''}`}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="w-full">
+          <Typography weight={500}>
+            <span className="text-[#464646] text-xs md:text-sm">
+              {testimonialData.desc}
+            </span>
+          </Typography>
         </div>
       </div>
-
-      <div className="w-full">
-        <Typography weight={500}>
-          <span className="text-[#464646] text-xs md:text-sm"></span>
-        </Typography>
-      </div>
-    </div>
+    </Card>
   )
 }
 
