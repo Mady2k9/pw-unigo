4e8e2e0a3f6d3675e65dd07ab381ff1981ff2309
Monkey D. Luffy <md.gauhar@pw.live>
2022-11-24 13:04:29 +0530

    FIxed demo video section of the landing page and wrapped all the text in typograpphy component

--- site/modules/k8/batch-details/classroom/topic-card/TopicCard.tsx
+++ site/modules/k8/batch-details/classroom/topic-card/TopicCard.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@components/ui'
+import { Card, Typography } from '@components/ui'
 import Image from 'next/image'
 import style from './TopicCard.module.css'
 import RightArrow from '@assets/images/right-arrow.svg'
@@ -7,17 +7,21 @@ const TopicCard = ({ handleClick }: { handleClick: () => void }) => {
   return (
     <Card>
       <div className={style.root} onClick={handleClick}>
-        <div className="text-2xl font-bold px-6 h-full flex items-center justify-center bg-[#EBE9F8]">
-          1
+        <div className="px-6 h-full flex items-center justify-center bg-[#EBE9F8]">
+          <Typography variant="heading3" weight={700}>
+            1
+          </Typography>
         </div>
         <div
           className={`flex items-center justify-between ${style.cardDetail}`}
         >
           <div className="flex flex-col justify-center">
-            <span className="text-base font-semibold">Coordinate Geometry</span>
-            <span className="text-xs font-medium text-[#8B8B8B]">
-              10 Lectures
-            </span>
+            <Typography variant="regular" weight={600}>
+              Coordinate Geometry
+            </Typography>
+            <Typography variant="tiny" weight={500}>
+              <span className="text-[#8B8B8B]">10 Lectures</span>
+            </Typography>
           </div>
           <div className={style.arrrowIcon}>
             <Image src={RightArrow} alt="logo" />
