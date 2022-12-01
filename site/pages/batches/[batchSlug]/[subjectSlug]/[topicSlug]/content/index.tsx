8d258cd197a2ef53d4e19df4d7fda4ce99ab5d6d
demonfox.pw <himanshu.sharma2@pw.live>
2022-12-01 13:00:30 +0530

    Updated Name

--- site/pages/batches/[batchSlug]/[subjectSlug]/[topicSlug]/content/index.tsx
+++ site/pages/batches/[batchSlug]/[subjectSlug]/[topicSlug]/content/index.tsx
@@ -1,30 +1,31 @@
-import { LayoutNoContentPadding } from '@components/common/Layout'
-import { LoadingSection, TabHeader } from '@components/ui'
-import { TabHeaderVariant } from '@components/ui/TabHeader/TabHeader'
+import {LayoutNoContentPadding} from '@components/common/Layout'
+import {LoadingSection, TabHeader} from '@components/ui'
+import {TabHeaderVariant} from '@components/ui/TabHeader/TabHeader'
 import useBatchContents, {
-  ContentType,
+    ContentType,
 } from '@lib/hooks/batches/useBatchContents'
-import { BatchType } from '@lib/hooks/batches/useBatches'
+import {BatchType} from '@lib/hooks/batches/useBatches'
 import dynamic from 'next/dynamic'
-import { useRouter } from 'next/router'
-import { Suspense, useEffect, useState } from 'react'
+import {useRouter} from 'next/router'
+import {Suspense, useEffect, useState} from 'react'
+import useBatchDetails from "@lib/hooks/batches/useBatchDetails";
 
 const TAB_ITEMS = {
-  selfLearning: {
-    items: [
-      { name: 'Lectures', key: ContentType.VIDEOS },
-      { name: 'Assignment', key: ContentType.NOTES },
-      { name: 'Practice', key: ContentType.EXERCISES },
-    ],
-  },
-  live: {
-    items: [
-      { name: 'Lectures', key: ContentType.VIDEOS },
-      { name: 'Notes', key: ContentType.NOTES },
-      { name: 'DHA', key: ContentType.DPP_NOTES },
-      { name: "DHA's Sol", key: ContentType.DPP_VIDEOS },
-    ],
-  },
+    selfLearning: {
+        items: [
+            {name: 'Lectures', key: ContentType.VIDEOS},
+            {name: 'Assignment', key: ContentType.NOTES},
+            {name: 'Practice', key: ContentType.EXERCISES},
+        ],
+    },
+    live: {
+        items: [
+            {name: 'Lectures', key: ContentType.VIDEOS},
+            {name: 'Notes', key: ContentType.NOTES},
+            {name: 'DHA', key: ContentType.DPP_NOTES},
+            {name: "DHA's Sol", key: ContentType.DPP_VIDEOS},
+        ],
+    },
 }
 
 const Lectures = dynamic(import('@modules/k8/content/lectures/Lectures'))
@@ -32,96 +33,107 @@ const Assignment = dynamic(import('@modules/k8/content/assignment/Assignment'))
 const Practice = dynamic(import('@modules/k8/content/practice/Practice'))
 const Notes = dynamic(import('@modules/k8/content/notes/Notes'))
 
-const Content = () => {
-  const tabHeaderVariant = TabHeaderVariant
-  const [currentIndex, setCurrentIndex] = useState<number>(0)
-  const router = useRouter()
+const ContentDetails = ({batch}: { batch: any }) => {
+    const tabHeaderVariant = TabHeaderVariant
+    const [currentIndex, setCurrentIndex] = useState<number>(0)
+    const router = useRouter()
 
-  const { batchSlug, subjectSlug, topicSlug, contentType } = router.query
+    const {batchSlug, subjectSlug, topicSlug, contentType} = router.query
 
-  const { data, isLoading, refetch } = useBatchContents({
-    batchSlug: batchSlug,
-    subjectSlug: subjectSlug,
-    contentType: TAB_ITEMS[contentType]?.items[currentIndex].key,
-    tag: topicSlug,
-  })
+    const {data, isLoading, refetch} = useBatchContents({
+        batchSlug: batchSlug as string,
+        subjectSlug: subjectSlug as string,
+        contentType: TAB_ITEMS[batch.contentType as BatchType]?.items[currentIndex].key,
+        tag: topicSlug as string,
+    })
 
-  useEffect(() => {
-    refetch
-  }, [currentIndex])
-  console.log(data)
+    useEffect(() => {
+        refetch
+    }, [currentIndex])
 
-  const renderItems = (data: any) => {
-    if (contentType === BatchType.SELF_LEARNING) {
-      switch (currentIndex) {
-        case 0:
-          return (
-            <Suspense fallback="loading...">
-              <Lectures videoData={data} />
-            </Suspense>
-          )
-        case 1:
-          return (
-            <Suspense fallback="loading...">
-              <Assignment />
-            </Suspense>
-          )
-        case 2:
-          return (
-            <Suspense fallback="loading...">
-              <Practice />
-            </Suspense>
-          )
-        default:
-          return <></>
-      }
-    } else {
-      switch (currentIndex) {
-        case 0:
-          return (
-            <Suspense fallback="loading...">
-              <Lectures videoData={data} />
-            </Suspense>
-          )
-        case 1:
-          return (
-            <Suspense fallback="loading...">
-              <Notes />
-            </Suspense>
-          )
-        case 2:
-          return (
-            <Suspense fallback="loading...">
-              <Notes />
-            </Suspense>
-          )
+    const renderItems = (data: any) => {
+        if (contentType === BatchType.SELF_LEARNING) {
+            switch (currentIndex) {
+                case 0:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Lectures videoData={data}/>
+                        </Suspense>
+                    )
+                case 1:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Assignment/>
+                        </Suspense>
+                    )
+                case 2:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Practice/>
+                        </Suspense>
+                    )
+                default:
+                    return <></>
+            }
+        } else {
+            switch (currentIndex) {
+                case 0:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Lectures videoData={data}/>
+                        </Suspense>
+                    )
+                case 1:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Notes/>
+                        </Suspense>
+                    )
+                case 2:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Notes/>
+                        </Suspense>
+                    )
 
-        case 3:
-          return (
-            <Suspense fallback="loading...">
-              <Lectures videoData={data} />
-            </Suspense>
-          )
-        default:
-          return <></>
-      }
+                case 3:
+                    return (
+                        <Suspense fallback="loading...">
+                            <Lectures videoData={data}/>
+                        </Suspense>
+                    )
+                default:
+                    return <></>
+            }
+        }
     }
-  }
 
-  return (
-    <div className="flex flex-col gap-6">
-      <TabHeader
-        title="Coordinate Geometry"
-        items={TAB_ITEMS[contentType].items}
-        currentIndex={currentIndex}
-        onChange={(index: number) => setCurrentIndex(index)}
-        variant={tabHeaderVariant.round}
-      />
-      {isLoading && <LoadingSection />}
+    return <div className="flex flex-col gap-6">
+        <TabHeader
+            title="Coordinate Geometry"
+            items={TAB_ITEMS[batch.contentType as BatchType].items}
+            currentIndex={currentIndex}
+            onChange={(index: number) => setCurrentIndex(index)}
+            variant={tabHeaderVariant.round}
+        />
+        {isLoading && <LoadingSection/>}
 
-      {data && renderItems(data)}
+        {data && renderItems(data)}
     </div>
-  )
+}
+const Content = () => {
+    const router = useRouter();
+    const {batchSlug} = router.query;
+    const {data: batchDetails, isLoading} = useBatchDetails({
+        batchSlug: batchSlug as string,
+    })
+
+    if (isLoading) {
+        return <LoadingSection/>
+    }
+    return (
+        <ContentDetails batch={batchDetails}/>
+    )
 }
 
 export default Content
