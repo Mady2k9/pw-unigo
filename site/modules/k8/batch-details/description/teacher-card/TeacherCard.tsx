151ad286f6bbaa958d5d8f06025c1ea8ee46e19d
Monkey D. Luffy <md.gauhar@pw.live>
2022-12-10 13:36:00 +0530

    feat: added lecture search and made teacher details modal responsive

--- site/modules/k8/batch-details/description/teacher-card/TeacherCard.tsx
+++ site/modules/k8/batch-details/description/teacher-card/TeacherCard.tsx
@@ -2,6 +2,8 @@ import TeacherPlaceholder from '@assets/images/default_teacher.svg'
 import Image from 'next/image'
 import { ArrowSmallRightIcon } from '@heroicons/react/24/outline'
 import { Typography } from '@components/ui'
+import { useState } from 'react'
+import TeacherModal from '../teacher-modal/TeacherModal'
 
 interface TeacherData {
   firstName: string
@@ -13,42 +15,55 @@ interface TeacherData {
 }
 
 function TeacherCard({ teacherData }: { teacherData: TeacherData }) {
+  const [modalOpen, setModalOpen] = useState(false)
   const teacherImg =
     (teacherData?.imageId?.baseUrl &&
       teacherData?.imageId?.key &&
       teacherData?.imageId?.baseUrl + teacherData?.imageId?.key) ||
     TeacherPlaceholder
   return (
-    <div className="animated fadeIn duration-200">
-      <div className="relative h-[136px] w-[157px] mx-auto">
-        <Image
-          src={teacherImg}
-          alt={`Banner Image`}
-          layout="fill"
-          objectFit="contain"
-          quality={100}
-        />
-      </div>
-      <div className="bg-[#F7F7F7] m-0 p-2 rounded-[8px] flex flex-col">
-        <Typography weight={800} variant="tiny" capitalize={true}>
-          <span className="text-gray-800">
-            {teacherData?.firstName || '' + ' ' + teacherData?.lastName || ''}
-          </span>
-        </Typography>
+    <>
+      <div
+        className="animated fadeIn duration-200"
+        onClick={() => setModalOpen(true)}
+      >
+        <div className="relative h-[136px] w-[157px] mx-auto">
+          <Image
+            src={teacherImg}
+            alt={`Banner Image`}
+            layout="fill"
+            objectFit="contain"
+            quality={100}
+          />
+        </div>
+        <div className="bg-[#F7F7F7] m-0 p-2 rounded-[8px] flex flex-col">
+          <Typography weight={800} variant="tiny" capitalize={true}>
+            <span className="text-gray-800">
+              {teacherData?.firstName || '' + ' ' + teacherData?.lastName || ''}
+            </span>
+          </Typography>
 
-        <div className={`flex items-center justify-between mt-1`}>
-          {['B.Tech', '9 Year Exp.'].map((meta, i) => (
-            <div
-              className="bg-white px-2 py-1 rounded-[6px] text-gray-800 text-[9px] font-bold"
-              key={i}
-            >
-              {meta}
-            </div>
-          ))}
-          <ArrowSmallRightIcon className={`w-4 text-[#6B4FE2]`} />
+          <div className={`flex items-center justify-between mt-1`}>
+            {['B.Tech', '9 Year Exp.'].map((meta, i) => (
+              <div
+                className="bg-white px-2 py-1 rounded-[6px] text-gray-800 text-[9px] font-bold"
+                key={i}
+              >
+                {meta}
+              </div>
+            ))}
+            <ArrowSmallRightIcon className={`w-4 text-[#6B4FE2]`} />
+          </div>
         </div>
       </div>
-    </div>
+
+      {modalOpen && (
+        <TeacherModal
+          teacherId={teacherData?._id}
+          onClose={() => setModalOpen(false)}
+        />
+      )}
+    </>
   )
 }
 
--- site/modules/k8/content/lectures/Lectures.tsx
+++ site/modules/k8/content/lectures/Lectures.tsx
@@ -1,43 +1,48 @@
-import {Container, LoadingSection, NoData, Typography} from '@components/ui'
+import { Container, LoadingSection, NoData, Typography } from '@components/ui'
 import style from './Lecture.module.css'
 import VideoCard from '../components/video-card/VideoCard'
-import {BatchType} from '@lib/hooks/batches/useBatches'
-import {useRouter} from 'next/router'
-import useBatchContents, {ContentType, Lecture} from '@lib/hooks/batches/useBatchContents'
-import {ModalSearch} from '@components/common'
-import {useEffect, useMemo, useState} from 'react'
-import {MagnifyingGlassIcon} from '@heroicons/react/20/solid'
+import { BatchType } from '@lib/hooks/batches/useBatches'
+import { useRouter } from 'next/router'
+import useBatchContents, {
+  ContentType,
+  Lecture,
+} from '@lib/hooks/batches/useBatchContents'
+import { ModalSearch } from '@components/common'
+import { useEffect, useMemo, useState } from 'react'
+import { MagnifyingGlassIcon } from '@heroicons/react/20/solid'
 import cn from 'clsx'
-import useBatchDetails from "@lib/hooks/batches/useBatchDetails";
+import useBatchDetails from '@lib/hooks/batches/useBatchDetails'
 
-
-const Lectures = ({ videoData, type }: { videoData?: Lecture[], type: ContentType }) => {
+const Lectures = ({ type }: { type: ContentType }) => {
   const router = useRouter()
   const { batchSlug, subjectSlug, topicSlug } = router.query
 
-  const {data: batchDetails} = useBatchDetails({batchSlug: batchSlug as string})
+  const { data: batchDetails } = useBatchDetails({
+    batchSlug: batchSlug as string,
+  })
 
   const variant = batchDetails?.isSelfLearning
-      ? BatchType.SELF_LEARNING
-      : BatchType.LIVE
+    ? BatchType.SELF_LEARNING
+    : BatchType.LIVE
 
-  const payload = topicSlug === 'all-contents' ? {
-    batchSlug: batchSlug as string as string,
-    subjectSlug: subjectSlug as string as string,
-    contentType: type as ContentType,
-  } : {
-    batchSlug: batchSlug as string as string,
-    subjectSlug: subjectSlug as string as string,
-    contentType: type as ContentType,
-    tag: topicSlug as string as string,
-  }
+  const payload =
+    topicSlug === 'all-contents'
+      ? {
+          batchSlug: batchSlug as string as string,
+          subjectSlug: subjectSlug as string as string,
+          contentType: type as ContentType,
+        }
+      : {
+          batchSlug: batchSlug as string as string,
+          subjectSlug: subjectSlug as string as string,
+          contentType: type as ContentType,
+          tag: topicSlug as string as string,
+        }
 
   const { data, isLoading } = useBatchContents(payload)
 
   const [enableSearch, setEnableSearch] = useState(false)
   const [query, setQuery] = useState('')
-  useEffect(() => {}, [query])
-
   const ItemsWrapper = useMemo(() => {
     return (
       <div
@@ -53,7 +58,7 @@ const Lectures = ({ videoData, type }: { videoData?: Lecture[], type: ContentTyp
       </div>
     )
   }, [data, enableSearch])
-  if(isLoading) return <LoadingSection />
+  if (isLoading) return <LoadingSection />
   if (data.length === 0) return <NoData />
 
   return (
@@ -61,7 +66,9 @@ const Lectures = ({ videoData, type }: { videoData?: Lecture[], type: ContentTyp
       {variant === BatchType.SELF_LEARNING && (
         <div
           className="relative text-gray-400 flex space-x-2 border rounded-md border-gray-200 px-4 py-2 shadow w-[400px] max-w-full"
-          onClick={() => setEnableSearch(true)}
+          onClick={() => {
+            setEnableSearch(true)
+          }}
         >
           <MagnifyingGlassIcon
             className="pointer-events-none h-5 w-5 "
@@ -74,11 +81,12 @@ const Lectures = ({ videoData, type }: { videoData?: Lecture[], type: ContentTyp
         <ModalSearch
           query={query}
           setQuery={setQuery}
-          onClose={() => setEnableSearch(false)}
-          itemsLength={videoData?.length || 0}
-        >
-          {ItemsWrapper}
-        </ModalSearch>
+          onClose={() => {
+            setEnableSearch(false)
+            setQuery('')
+          }}
+          videos={data as Lecture[]}
+        />
       ) : (
         ItemsWrapper
       )}
