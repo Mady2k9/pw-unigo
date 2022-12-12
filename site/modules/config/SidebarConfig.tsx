61658febb3339af46513fcde8279c249e8461895
demonfox.pw <himanshu.sharma2@pw.live>
2022-12-12 20:42:11 +0530

    Updated Name

--- site/modules/config/SidebarConfig.tsx
+++ site/modules/config/SidebarConfig.tsx
@@ -1,32 +1,37 @@
 import {
     AcademicCapIcon,
-    ArrowTopRightOnSquareIcon,
-    BookmarkIcon,
-    ComputerDesktopIcon,
     HomeIcon
 } from "@heroicons/react/24/outline";
 import React from "react";
 
+export const ShowCohortOn = '/';
 const SidebarConfig = [
     [
         {
             name: 'Home',
             icon: <HomeIcon className={'h-5 font-bold'}/>,
+            pattern: /\/*/,
             route: '/',
         },
         {
             name: 'About Us',
             icon: <AcademicCapIcon className={'h-5 font-bold'}/>,
+
+            pattern: /\/about-us/,
             route: '/about-us',
         },
         {
             name: 'Contact Us',
             icon: <AcademicCapIcon className={'h-5 font-bold'}/>,
+
+            pattern: /\/contact-us/,
             route: '/contact-us',
         },
         {
             name: 'Terms & Conditions',
             icon: <AcademicCapIcon className={'h-5 font-bold'}/>,
+
+            pattern: /\/terms-and-conditions/,
             route: '/terms-and-conditions',
         },
     ],
--- site/modules/k8/home/testimonials/testimonial-section/TestimonialSection.tsx
+++ site/modules/k8/home/testimonials/testimonial-section/TestimonialSection.tsx
@@ -1,48 +1,31 @@
-import { LoadingSection, Typography } from '@components/ui'
+import {LoadingSection, Typography} from '@components/ui'
 import useTestimonials from '@lib/hooks/batches/useCohortTestimonials'
 import TestimonialCard from '../testimonial-card/TestimonialCard'
 import style from './TestimonialSection.module.css'
 
-const TESTIMONIAL_DATA = [
-  {
-    id: 1,
-    name: 'Father of Ayush',
-    desc: 'This PW App is really Nice. Really Helping My Son Soo Much. Actually Childrens rather than Simply studying, They like to visualize the things',
-  },
-  {
-    id: 2,
-    name: 'Father of Amir',
-    desc: 'This PW App is really Nice. Really Helping My Son Soo Much. Actually Childrens rather than Simply studying, They like to visualize the things',
-  },
-  {
-    id: 3,
-    name: 'Father of Vikash',
-    desc: 'This PW App is really Nice. Really Helping My Son Soo Much. Actually Childrens rather than Simply studying, They like to visualize the things',
-  },
-]
-
 const TestimonialSection = () => {
-  const { data, isLoading } = useTestimonials({
-    cohortId: process.env.NEXT_PUBLIC_K8_COHORT_ID as string,
-  })
-  if (isLoading) return <LoadingSection />
+    const {data, isLoading} = useTestimonials({
+        cohortId: process.env.NEXT_PUBLIC_K8_COHORT_ID as string,
+    })
+    if (isLoading) return <LoadingSection/>
 
-  const { testimonialsData } = data
-  if (testimonialsData.length === 0) return <></>
-  return (
-    <div className="flex flex-col gap-9 md:gap-11 w-full mt-0 md:mt-16 ">
-      <Typography variant="heading3" weight={700}>
-        Parent's Voice
-      </Typography>
+    const {testimonialsData} = data
+    if (testimonialsData.length === 0) return <></>
+    return (
+        <div className="flex flex-col gap-4 md:gap-6 w-full mt-0 md:mt-16 ">
+            <Typography variant="heading3" weight={700}>
+                Parent's Voice
+            </Typography>
 
-      <div className={style.cardContainer}>
-        {testimonialsData &&
-          testimonialsData.map((data: any, idx: number) => (
-            <TestimonialCard key={data._id} testimonialData={data} />
-          ))}
-      </div>
-    </div>
-  )
+            <div className={style.cardContainer}>
+                {
+                    testimonialsData &&
+                    testimonialsData.map((data: any, idx: number) => (
+                        <TestimonialCard key={data._id} testimonialData={data}/>
+                    ))}
+            </div>
+        </div>
+    )
 }
 
 export default TestimonialSection
