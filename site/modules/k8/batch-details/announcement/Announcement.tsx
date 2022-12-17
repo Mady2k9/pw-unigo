da930e7b42f5af19ebe8baecb6f8fa13167865d4
demonfox.pw <himanshu.sharma2@pw.live>
2022-12-17 22:55:10 +0530

    Added Announcement

--- site/modules/k8/batch-details/announcement/Announcement.tsx
+++ site/modules/k8/batch-details/announcement/Announcement.tsx
@@ -1,16 +1,20 @@
 import useAnnouncement from "@lib/hooks/batches/useAnnouncement";
-import {Container, LoadingSection} from "@components/ui";
+import {Container, LoadingSection, NoData} from "@components/ui";
 import {AnnouncementCard} from "@components/common";
 
 const Announcement = ({batchDetails}: { batchDetails: any }) => {
     const {data, isLoading} = useAnnouncement({batchSlug: batchDetails?._id});
-    console.log(data)
     return <Container>
         <div className={'flex flex-col space-y-8'}>
             {
                 isLoading ? <LoadingSection/> : data.map((announcement, index) => {
-                    return <AnnouncementCard key={index} attachment={announcement.attachment} createdAt={announcement.createdAt} announcement={announcement.announcement}/>
+                    return <AnnouncementCard key={index} attachment={announcement.attachment}
+                                             createdAt={announcement.createdAt}
+                                             announcement={announcement.announcement}/>
                 })
+            }
+            {
+                !isLoading && !data?.length && <NoData/>
             }
         </div>
     </Container>
