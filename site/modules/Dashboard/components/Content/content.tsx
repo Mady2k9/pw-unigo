9f5417071e13c15ba7906307be6724111b116b90
digdev <digdev.singh@pw.live>
2023-06-27 11:22:57 +0530

    Sidebar

--- site/modules/Dashboard/components/Content/content.tsx
+++ site/modules/Dashboard/components/Content/content.tsx
@@ -7,8 +7,12 @@ export interface contentProps {}
 
 const content: React.FC<contentProps> = (props) => {
   return (
-    <div className="w-full bg-white h-[calc(100vh-80px)] overflow-scroll sm:p-4">
-      <div>Content Area</div>
+    <div className="w-full bg-white h-[calc(100vh-80px)] sm:p-4">
+      <div className="w-full ">
+        <div className="flex sm:w-10/12 w-full sm:justify-between sm:mx-auto justify-end ">
+          asdasd
+        </div>
+      </div>
     </div>
   )
 }
