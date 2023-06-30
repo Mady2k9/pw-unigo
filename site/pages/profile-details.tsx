177a926164181f80c481c91c1581c878b8214e8c
Uttam <uttam.sharma@pw.live>
2023-06-30 15:52:07 +0530

    profile-update

--- site/pages/profile-details.tsx
+++ site/pages/profile-details.tsx
@@ -3,16 +3,7 @@ import Sidebar from '@modules/Dashboard/components/Sidebar/sidebar'
 import Content from '@modules/Dashboard/components/Content/content'
 import ProfileView from '@modules/auth/components/ProfileView'
 const profileDetails = () => {
-  return (
-    <>
-      <ProfileView onSubmit={undefined} />
-      <Header title="Step 1: Profile Details" />
-      <div className="sticky left-0 h-[calc(100vh-80px)] bg-[#f8f8f8] z-19 sm:flex">
-        <Sidebar name="John Snow" phone="+91 7864546474" />
-        <Content></Content>
-      </div>
-    </>
-  )
+  return <ProfileView />
 }
 
 export default profileDetails
