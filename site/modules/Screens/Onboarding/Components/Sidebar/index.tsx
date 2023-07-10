362c80fa04fb76463ff38af9e7e8ce6438777edc
digdev <digdev.singh@pw.live>
2023-07-10 16:17:52 +0530

    upload documents

--- site/modules/Screens/Onboarding/Components/Sidebar/index.tsx
+++ site/modules/Screens/Onboarding/Components/Sidebar/index.tsx
@@ -41,39 +41,38 @@ const sidebar: React.FC<sidebarProps> = (props) => {
 
   return (
     <>
-      <div className="sm:w-[235px] z-40 flex pt-4 sm:justify-center w-full">
-        <div className={s.mainContainer}>
-          <div className={s.container}>
-            <img
-              className="mx-auto"
-              src="/user_profile.svg"
-              alt="user profile"
-            />
-            <p className="text-[16px] ">{name}</p>
-            <div className="flex justify-center items-center">
-              <span className="stroke-black">
-                <img className="mr-2" src="/phone.svg" alt="phone" />
-              </span>
-              <p className="text-[12px] text-[#757575]">{phone}</p>
-            </div>
-          </div>
-          <div className="flex sm:flex-row flex-col items-center sm:pb-0 pb-4">
-            <div className="flex sm:flex-col flex-row sm:order-2 sm:gap-0 gap-3">
-              <div
-                className="mb-2 sm:text-left text-center"
-                onClick={goToProfilePage}
-              >
-                <span className={s.step_text_active}>Step 1</span>
-                <div className={s.icon_container}>
-                  <img className={s.step_img} src="/step_1c.svg" alt="step1" />
-                  <span className={s.step_icon_text}>Profile Details</span>
-                </div>
+      <div className="sticky">
+        <div className=" sm:w-[235px] z-40 flex pt-4 sm:justify-center w-full">
+          <div className={s.mainContainer}>
+            <div className={s.container}>
+              <img
+                className="mx-auto"
+                src="/user_profile.svg"
+                alt="user profile"
+              />
+              <p className="text-[16px] ">{name}</p>
+              <div className="flex justify-center items-center">
+                <span className="stroke-black">
+                  <img className="mr-2" src="/phone.svg" alt="phone" />
+                </span>
+                <p className="text-[12px] text-[#757575]">{phone}</p>
               </div>
+            </div>
+            <div className="flex sm:flex-row flex-col items-center sm:pb-0 pb-4">
+              <div className="flex sm:flex-col flex-row sm:order-2 sm:gap-0 gap-3">
+                <div className="mb-2 sm:text-left text-center">
+                  <span className={s.step_text_active}>Step 1</span>
+                  <div className={s.icon_container}>
+                    <img
+                      className={s.step_img}
+                      src="/step_1c.svg"
+                      alt="step1"
+                    />
+                    <span className={s.step_icon_text}>Profile Details</span>
+                  </div>
+                </div>
 
-              <div
-                className="mb-2 sm:text-left text-center"
-                onClick={goToNominatePage}
-              >
+              <div className="mb-2 sm:text-left text-center">
                 <span className={s.step_text}>Step 2</span>
                 <div className={s.icon_container}>
                   <img className={s.step_img} src="/step_2c.svg" alt="step2" />
@@ -126,94 +125,6 @@ const sidebar: React.FC<sidebarProps> = (props) => {
             </div>
           </div>
         </div>
-        {isProfilePageOpened === true ? (
-          <div className="opacity-25 fixed inset-0 z-40 bg-[#414347] "></div>
-        ) : (
-          ''
-        )}
-        {isProfilePageOpened === true ? (
-          <Dialog
-            className={'relative z-[999999]'}
-            open={isProfilePageOpened}
-            onClose={toggleModal}
-          >
-            <div className="fixed inset-0 bg-black/20" aria-hidden="true" />
-            <div className="fixed inset-0 flex items-center justify-center p-4 ">
-              <Dialog.Panel className="mx-auto w-full max-w-[480px] max-h-[212px] rounded-xl bg-white ring-1 transition-all p-5 relative">
-                <div
-                  className="cursor-pointer absolute top-4 right-4"
-                  onClick={toggleModal}
-                >
-                  <Cross className="h-6 w-6" />
-                </div>
-                <div className="text-center m-2">
-                  <p className="font-bold text-[20px] ">
-                    Going to previous step?
-                  </p>
-                  <p className="text-[16px] mt-[20px]">
-                    Are you sure you want to go back without completing this
-                    step?{' '}
-                  </p>
-                </div>
-                <div className="flex justify-center mt-6 text-[16px] font-[600px]">
-                  <button
-                    onClick={onSubmitProfile}
-                    className="w-[208px] h-[48px] border border-[#5A4BDA] rounded-md text-[#5A4BDA]"
-                  >
-                    Go to Profile details
-                  </button>
-                  <button
-                    onClick={toggleModal}
-                    className="w-[208px] h-[48px] border ml-6 bg-[#5A4BDA] text-white rounded-md"
-                  >
-                    Complete it first
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </div>
-          </Dialog>
-        ) : (
-          <Dialog
-            className={'relative z-[999999]'}
-            open={nominateAgain}
-            onClose={toggleModal}
-          >
-            <div className="fixed inset-0 bg-black/20" aria-hidden="true" />
-            <div className="fixed inset-0 flex items-center justify-center p-4 ">
-              <Dialog.Panel className="mx-auto w-full max-w-[480px] max-h-[212px] rounded-xl bg-white ring-1 transition-all p-5 relative">
-                <div
-                  className="cursor-pointer absolute top-4 right-4"
-                  onClick={toggleModal}
-                >
-                  <Cross className="h-6 w-6" />
-                </div>
-                <div className="text-center m-2">
-                  <p className="font-bold text-[20px] ">
-                    Going to previous step?
-                  </p>
-                  <p className="text-[16px] mt-[20px]">
-                    Are you sure you want to go back without completing this
-                    step?{' '}
-                  </p>
-                </div>
-                <div className="flex justify-center mt-6 text-[16px] font-[600px]">
-                  <button
-                    onClick={onSubmitNominate}
-                    className="w-[208px] h-[48px] border border-[#5A4BDA] rounded-md text-[#5A4BDA]"
-                  >
-                    Go to nomination form
-                  </button>
-                  <button
-                    onClick={toggleModal}
-                    className="w-[208px] h-[48px] border ml-6 bg-[#5A4BDA] text-white rounded-md"
-                  >
-                    Complete it first
-                  </button>
-                </div>
-              </Dialog.Panel>
-            </div>
-          </Dialog>
-        )}
       </div>
       {show === true ? <ImportantNoticeData /> : ''}
     </>
