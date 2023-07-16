96d3389add418d26b5260945a50314e8b0405519
Aditya_Singh_Rathore <aditya.rathore@pw.live>
2023-07-16 11:15:23 +0530

    Marvel WIP

--- site/modules/Screens/Onboarding/UploadDocuments/index.tsx
+++ site/modules/Screens/Onboarding/UploadDocuments/index.tsx
@@ -1,14 +1,34 @@
 import { Header } from '@modules/Screens/Onboarding/Components'
 import DocumentsSection from '@modules/Screens/Onboarding/UploadDocuments/DocumentsSection'
 import Layout from '../Layout'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { postFormData } from '@modules/auth/lib'
 import { toast } from 'react-hot-toast'
 import { useRouter } from 'next/router'
+import { useGetDraftData } from '@lib/hooks/marvel/useGetDraftData'
+import { useMarvelContext } from '@modules/MarvelContext'
 
 const UploadDocumentsScreen = () => {
   const [studentDocsInfo, setStudentDocsInfo] = useState<any>({})
   const [nominationDocsInfo, setNominationDocsInfo] = useState<any>([])
+  const { draftData } = useGetDraftData()
+  const { completedStepTill, updateCompletedSteps } = useMarvelContext()
+
+  useEffect(() => {
+      const nominationDocsInfo= draftData?.pwMarvelData?.nominationDocsInfo
+      const uploadDocsInfo = draftData?.pwMarvelData?.uploadDocsInfo
+      const studentDocsInfo = draftData?.pwMarvelData?.studentDocsInfo
+      const step = uploadDocsInfo ? 3 : nominationDocsInfo ? 2 : 1
+      updateCompletedSteps(Math.max(completedStepTill, step))
+
+      if (studentDocsInfo) {
+        setStudentDocsInfo(studentDocsInfo)
+      } 
+
+      if (nominationDocsInfo) {
+        setNominationDocsInfo(nominationDocsInfo)
+      }
+  }, [draftData, completedStepTill, updateCompletedSteps])
 
   const { push } = useRouter()
 
@@ -33,7 +53,6 @@ const UploadDocumentsScreen = () => {
   } */
 
   const onSubmit = () => {
-    console.log('submitting the form')
     const randomId = localStorage.getItem('randomId') || ''
     const dataToSend = {
       studentDocsInfo,
@@ -49,7 +68,16 @@ const UploadDocumentsScreen = () => {
   }
 
   const onNominationDocumentUpload = (data: any) => {
-    setNominationDocsInfo([...nominationDocsInfo, data])
+    const updatedDocInfo = nominationDocsInfo?.map((el: any) => {
+      if (
+        (el?.exam === data?.exam) && (el?.year == data?.year) && el?.criteria === data?.criteria
+      ) {
+        el.achivementId = data?.achivementId
+      }
+
+      return el
+    })
+    setNominationDocsInfo([...updatedDocInfo])
   }
 
   const onStudentDocUpload = (key: string, id: string) => {
@@ -62,6 +90,15 @@ const UploadDocumentsScreen = () => {
         adhaarInfo: { ...studentDocsInfo.adhaarInfo, [objectKeys[1]]: id },
       })
     }
+  }
+  const shouldSubmitDisable = () => {
+    const checkStudentDoc = (studentDocsInfo?.adhaarInfo?.adhaarFrontPage)
+                            && (studentDocsInfo?.adhaarInfo?.adhaarBackPage)
+                            && (studentDocsInfo?.passportPhoto)
+                            && (studentDocsInfo?.reportCard)
+
+    const checkForNominationDocs = draftData?.nominationDocsInfo?.every((el: any) => el?.achivementId)
+    return !(checkStudentDoc && checkForNominationDocs)
   }
 
   return (
@@ -76,6 +113,8 @@ const UploadDocumentsScreen = () => {
           profileData={undefined}
           isEditEnabled={false}
           navBarText={''}
+          shouldDisabled={shouldSubmitDisable()}
+          hideSubmitButton={draftData?.isRegistrationEnded}
         />
       }
     >
--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,8 +1,18 @@
 import Footer from '@modules/SiteFooter'
 import Header from '@components/common/Header/Header'
 import { Banner, Faq, RewardsCard, NominationSteps } from 'modules'
+import { useEffect } from 'react'
+import { useUI } from '@components/ui'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const { user } = useUI()
+  const router = useRouter()
+  useEffect(() => {
+    if (user) {
+        router.replace('/profile-details')
+    }
+}, [user])
   return (
     <>
       <Header variant="MARVELSHeader" />
