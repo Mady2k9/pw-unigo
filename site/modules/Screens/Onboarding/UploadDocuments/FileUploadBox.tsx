a1c13a467182ee8621b7ea3c2391443f6090a3a6
Aditya_Singh_Rathore <aditya.rathore@pw.live>
2023-07-18 14:06:27 +0530

    updated trim function

--- site/modules/Screens/Onboarding/UploadDocuments/FileUploadBox.tsx
+++ site/modules/Screens/Onboarding/UploadDocuments/FileUploadBox.tsx
@@ -217,8 +217,8 @@ const FileSelected = ({
   )
 }
 
-const textTrim = (text: string) => {
-  return text?.slice(0, 4) + '...' + text.slice(text?.length  - 6)
+const textTrim = (text: string = '') => {
+  return text?.length > 10 ? text?.slice(0, 4) + '...' + text.slice(text?.length  - 6) : text
 }
 
 const FileUploaded = ({
