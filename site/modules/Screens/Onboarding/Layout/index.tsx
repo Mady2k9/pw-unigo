d8b7a07ba4aa434527275910ab6353acffdf3b37
Ujjwal <ujjwal@pw.live>
2023-07-27 17:54:33 +0530

    important notices changes

--- site/modules/Screens/Onboarding/Layout/index.tsx
+++ site/modules/Screens/Onboarding/Layout/index.tsx
@@ -9,7 +9,12 @@ type LayoutProps = {
   isRegistrationEnded: boolean
 }
 
-const Layout = ({ children, header, navBarText, isRegistrationEnded }: LayoutProps) => {
+const Layout = ({
+  children,
+  header,
+  navBarText,
+  isRegistrationEnded,
+}: LayoutProps) => {
   const [userData, setUserData] = useState<any>({})
   const [show, setShow] = useState(false)
 
@@ -23,6 +28,16 @@ const Layout = ({ children, header, navBarText, isRegistrationEnded }: LayoutPro
     setShow(!show)
   }
 
+  const [showModal, setShowModal] = useState(false)
+  const showDataModal = () => {
+    console.log('show modal fire', showModal)
+    setShowModal(!showModal)
+
+    /*  if (showModal) {
+      setShowModal(false)
+    } */
+  }
+
   const name = useMemo(() => {
     return userData?.firstName || '' + userData?.lastName
   }, [userData?.firstName, userData?.lastName])
@@ -30,7 +45,7 @@ const Layout = ({ children, header, navBarText, isRegistrationEnded }: LayoutPro
   return (
     <>
       {header}
-      {show === true ? <ImportantNoticeData /> : ''}
+      {show === true ? <ImportantNoticeData closeModal={showDataModal} /> : ''}
 
       <div className="sticky left-0 h-[calc(100vh-80px)] sm:h-100vh sm:bg-[#f8f8f8] bg-white  z-19 sm:flex mt-[48px] sm:mt-0">
         <Sidebar
