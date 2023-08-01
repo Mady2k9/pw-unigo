4573c67ff04e985346e8fc03a05e1c3cbc0603cb
amandeep.singh <Amandeep.singh@pw.live>
2023-08-01 14:26:52 +0530

    banner fixes

--- site/modules/TopUniversity/topUnivaersity.tsx
+++ site/modules/TopUniversity/topUnivaersity.tsx
@@ -5,10 +5,10 @@ function topUnivaersity() {
     <>
 
 
-<div>
+<div className='relative'>
     <img className='w-full sm:block hidden ' src="hero.svg" alt="top banner" />
     <img className='w-full sm:hidden block' src="hero_mweb.svg" alt="Top banner" />
-  <div className="absolute top-[15%] md:top-[2%] lg:top-[3%] xl:top-[15%] 2xl:top-[20%] font-[700] justify-center text-center w-full">
+  <div className="absolute top-[20%] md:bottom-[10px] lg:top-[3%] xl:top-[15%] 2xl:top-[20%] font-[700] justify-center text-center w-full">
     <div className='lg:w-[824px] md:w-[500px] w-[328px] m-auto'>
   <p className='lg:text-[64px] md:text-[30px] text-[32px] font-[600] leading-[48px] sm:leading-[24px] lg:leading-[80px]'>Study Abroad from</p>
     <p className='lg:text-[64px] sm:text-[28px] text-[32px] font-[600] leading-[48px] lg:leading-[80px] text-[#DA1F3D]'>Top Universities</p>
