8b326e3cf8a5bdb8ec6af77d178417a861b38b73
amandeep.singh <Amandeep.singh@pw.live>
2023-08-01 13:03:07 +0530

    done

--- site/modules/GlobalEducation/globalEducation.tsx
+++ site/modules/GlobalEducation/globalEducation.tsx
@@ -1,9 +1,4 @@
 import React from 'react'
-import Image from 'next/image'
-import FirstCardImage from '../../public/why_choose_icon1.svg'
-import SecondCardImage from '../../public/why_choose_icon2.svg'
-import ThirdCardImage from '../../public/why_choose_icon3.svg'
-import FourthCardImage from '../../public/why_choose_icon4.svg'
 
 function globalEducation() {
   return (
@@ -20,56 +15,56 @@ function globalEducation() {
 
       <div className="lg:flex justify-center gap-2 ">
         <div className="px-[16px] sm:p-0">
-          <div className="border border-[#B7B7B7] md:p-[24px] p-[16px] flex mb-2 items-center gap-[24px] h-[104px] sm:h-[136px] ">
+          <div className="border border-[#B7B7B7] p-[16px] flex mb-2 items-center gap-[24px] ">
               <img className='w-[56px] h-[56px]' src="why_choose_icon1.svg" alt="demo" />
-            <div>
-              <p className="sm:text-[20px] text-[16px] font-bold leading-[24px] mb-[4px] sm:mb-[10px]">
+            <div className='flex flex-col lg:gap-y-[10px] gap-y-[4px]'>
+              <p className="sm:text-[20px] text-[16px] font-bold leading-[24px]">
                 One to one counselling
               </p>
-              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-medium leading-[22px]">
+              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-[500] leading-[22px]">
                 Get personalised help from the best mentors in the country
               </p>
             </div>
           </div>
-          <div className="border border-[#B7B7B7] md:p-[24px] p-[16px] flex items-center gap-[24px] h-[104px] sm:h-[136px] mb-2">
+          <div className="border border-[#B7B7B7] p-[16px] flex items-center gap-[24px] mb-2">
            
               <img className='w-[56px] h-[56px]' src="why_choose_icon2.svg" alt="demo" />
         
-            <div>
-              <p className="sm:text-[20px] text-[16px] font-[700] leading-[24px] mb-[4px] sm:mb-[10px]">
+            <div className='flex flex-col lg:gap-y-[10px] gap-y-[4px]'>
+              <p className="sm:text-[20px] text-[16px] font-[700] leading-[24px]">
               Academic Program
               </p>
-              <div className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-medium leading-[22px]">
+              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-[500] leading-[22px]">
               PW Unigo provides preparation modules for you to ace standardized tests
-              </div>
+              </p>
             </div>
           </div>
         </div>
-        <div className="px-[16px] sm:p-0">
-          <div className="border border-[#B7B7B7] md:p-[24px] p-[16px] flex mb-2 items-center gap-[24px] h-[104px] sm:h-[136px]">
+        <div className="px-[16px] sm:p-0 ">
+          <div className="border border-[#B7B7B7]  p-[16px] flex mb-2 items-center gap-[24px]">
        
               <img className='w-[56px] h-[56px]' src="why_choose_icon3.svg" alt="demo" />
          
-            <div>
-              <p className="sm:text-[20px] text-[16px] font-[700] sm:leading-[30px] leading-[24px] mb-[4px] sm:mb-[10px]">
+            <div className='flex flex-col lg:gap-y-[10px] gap-y-[4px]' >
+              <p className="sm:text-[20px] text-[16px] font-[700] leading-[24px]">
               Student Community
               </p>
-              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-medium leading-[22px]">
+              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-[500] leading-[22px]">
               Discover and connect with like-minded peers in your field of interest
               </p>
             </div>
           </div>
-          <div className="border border-[#B7B7B7] md:p-[24px] p-[16px] flex items-center gap-[24px] h-[104px] sm:h-[136px]">
+          <div className="border border-[#B7B7B7] p-[16px] flex items-center gap-[24px]">
          
               <img className='w-[56px] h-[56px]' src="why_choose_icon4.svg" alt="demo" />
       
-            <div>
-              <p className="sm:text-[20px] text-[16px] font-[700] leading-[24px] mb-[4px] sm:mb-[10px]">
+            <div className='flex flex-col lg:gap-y-[10px] gap-y-[4px]'>
+              <p className="sm:text-[20px] text-[16px] font-[700] leading-[24px]">
               Complete Support
               </p>
-              <div className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-medium leading-[22px]">
+              <p className="sm:text-[16px] text-[14px] text-[#3D3D3D] font-[500] leading-[22px]">
               Get complete support and guidance throughout the entire duration of your course
-              </div>
+              </p>
             </div>
           </div>
         </div>
