9683a05485dbf28b494ca0b85a10da740d3e1a8f
Uttam <uttam.sharma@pw.live>
2023-08-02 20:00:20 +0530

    dd

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,17 +1,5 @@
-import { Footer } from '@components/common'
-import Header from '@components/common/Header/Header'
-import { Loader, Typography } from '@components/ui'
-
-import About from '@modules/components/About'
-import { Features } from '@modules/components/Features'
-import Hero from '@modules/components/Hero'
-import School from '@modules/components/School/School'
-import Why from '@modules/components/Why'
-
 const Home = () => {
-  return (
-   <h1>PW UNIGO</h1>
-  )
+  return <h1>PW UNIGO</h1>
 }
 
 export default Home
