9002d40d1ca1f2bf2770749ab9e8965a6891ca83
Uttam <uttam.sharma@pw.live>
2023-08-03 16:29:57 +0530

    faq

--- site/pages/privacy-policy.tsx
+++ site/pages/privacy-policy.tsx
@@ -1,14 +1,58 @@
-import Header from '../modules/Header'
-import Footer from '../modules/Footer'
+import Header from '@modules/Header'
+import Footer from '@modules/Footer'
 import PrivacyPolicy from '../modules/PrivacyPolicy'
 import Faq from '@modules/Faq'
+
+const items = [
+  {
+    title: 'Who is eligible to study MBBS in medical colleges abroad?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title: 'Can I practice in India upon my return?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title:
+      'Will I be eligible for job opportunities in Government Hospitals or allied medical institutions in India?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title:
+      'What will be the total expenditure, including MBBS fees? Can I obtain a bank loan for my education, and to what extent?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title: 'Where can I do my internship during my MBBS studies?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title: 'What is the procedure for admission to study MBBS abroad?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+  {
+    title: 'What will be the total expenditure, including MBBS fees?',
+    description:
+      'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  },
+]
 
 const Home = () => {
   return (
     <>
       <Header handleState={undefined} />
       <PrivacyPolicy />
-      <Faq data={undefined} />
+      <Faq
+        items={items}
+        heading="You have questions. We have answers!"
+        subheading="Check out the most commonly asked questions and their answers."
+      />
       <Footer />
     </>
   )
