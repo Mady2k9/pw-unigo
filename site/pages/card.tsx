ff806c3e858c6eb2b1d7707bfe360688e5a91f34
Uttam <uttam.sharma@pw.live>
2023-08-04 14:01:32 +0530

    coma.svg path

--- site/pages/card.tsx
+++ site/pages/card.tsx
@@ -1,26 +1,24 @@
-import Coma from './assets/coma.svg'
-import Image from 'next/image';
-const Card = ({name, comment}:{name:string , comment : string}) => {
-    return ( <>
-    <div className=" w-[300px] h-[280px] md:h-[314px] mr-[16px] md:mr-[20px] md:w-[360px]  p-[16px] md:p-[24px] bg-white">
-        <div className='w-[40px] h-[30px] mb-[14px] '>
-            <Image src={Coma} alt='quote icon'/>
+import Coma from '@assets/images/coma.svg'
+import Image from 'next/image'
+const Card = ({ name, comment }: { name: string; comment: string }) => {
+  return (
+    <>
+      <div className=" w-[300px] h-[280px] md:h-[314px] mr-[16px] md:mr-[20px] md:w-[360px]  p-[16px] md:p-[24px] bg-white">
+        <div className="w-[40px] h-[30px] mb-[14px] ">
+          <Image src={Coma} alt="quote icon" />
         </div>
-        <div className='text-[14px] md:text-[16px] mb-[24px]'>
-            Studying abroad with [Study Abroad Program Name] was a
-            life-changing experience for me. 
-            The program provided an exceptional
-            blend of academic excellence, cultural immersion,
-            and personal growth.
+        <div className="text-[14px] md:text-[16px] mb-[24px]">
+          Studying abroad with [Study Abroad Program Name] was a life-changing
+          experience for me. The program provided an exceptional blend of
+          academic excellence, cultural immersion, and personal growth.
         </div>
-        <div className='text-[16px] md:text-[18px] font-[700]'>
-            {name}
+        <div className="text-[16px] md:text-[18px] font-[700]">{name}</div>
+        <div className="text-[#757575] text-[14px] md:text-[16px]">
+          {comment}
         </div>
-        <div className='text-[#757575] text-[14px] md:text-[16px]'>
-            {comment}
-        </div>
-    </div>
-    </> );
+      </div>
+    </>
+  )
 }
- 
-export default Card;
+
+export default Card
