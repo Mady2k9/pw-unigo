947851276899d93daf5e3fd8246449ef966476bd
digdev <digdev.singh@pw.live>
2023-08-07 17:49:48 +0530

    faq on privacy page

--- site/pages/privacy-policy.tsx
+++ site/pages/privacy-policy.tsx
@@ -1,40 +1,40 @@
 import Header from '../modules/Header'
 import Footer from '../modules/Footer'
 import PrivacyPolicy from '../modules/PrivacyPolicy'
-import Faq from '@modules/Faq'
+// import Faq from '@modules/Faq'
 
 const Home = () => {
-  const items = [
-    {
-      title: 'How do I Share my Referral Code?',
-      description:
-        'Just Click on Refer & Earn on Menu of App, Click on the share button',
-    },
-    {
-      title: 'How do I Share my Referral Code?',
-      description:
-        'Just Click on Refer & Earn on Menu of App, Click on the share button',
-    },
-    {
-      title: 'How do I Share my Referral Code?',
-      description:
-        'Just Click on Refer & Earn on Menu of App, Click on the share button',
-    },
-    {
-      title: 'How do I Share my Referral Code?',
-      description:
-        'Just Click on Refer & Earn on Menu of App, Click on the share button',
-    },
-  ]
+  // const items = [
+  //   {
+  //     title: 'How do I Share my Referral Code?',
+  //     description:
+  //       'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  //   },
+  //   {
+  //     title: 'How do I Share my Referral Code?',
+  //     description:
+  //       'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  //   },
+  //   {
+  //     title: 'How do I Share my Referral Code?',
+  //     description:
+  //       'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  //   },
+  //   {
+  //     title: 'How do I Share my Referral Code?',
+  //     description:
+  //       'Just Click on Refer & Earn on Menu of App, Click on the share button',
+  //   },
+  // ]
 
   return (
     <>
       <Header handleState={undefined} />
       <PrivacyPolicy />
-      <Faq
+      {/* <Faq
         items={items}
         subheading="Check out the most commonly asked questions and their answers."
-      />
+      /> */}
       <Footer />
     </>
   )
